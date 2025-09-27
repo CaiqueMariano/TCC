0cb2f1653ed5e64a7e1369280956cb7660b526cb
NicolasSouzaAguiar <nicolasssouzaaaguiarr@gmail.com>
2025-09-26 23:41:40 -0300

    Home

--- APP/Zeloo/src/screens/Home.js
+++ APP/Zeloo/src/screens/Home.js
@@ -44,169 +44,152 @@ export default function Home ({ navigation, route }) {
   }, [user]);
 
   return (
-    <View style={styles.container}>
+<View style={styles.container}>
+  <View style={styles.Form1}></View>
+  <Image 
+    source={require('../../assets/images/Zeloo.png')}
+    style={styles.Logo}
+  />
 
-    <View style={styles.header}>
-      <Image source={require('../../assets/images/logo.png')} style={styles.logo} />
-      <TouchableOpacity onPress={() => alert('auxiliar auditivo')}>
-        <Image source={require('../../assets/images/volume.png')} style={styles.som} />
+  <ScrollView
+    contentContainerStyle={{
+      flexGrow: 1,
+      alignItems: 'center',
+      paddingVertical: 20,
+    }}
+  >
+    <View style={styles.grid}>
+      {/* BOTÃO CUIDADORES */}
+      <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('Contratar.js')}>
+        <Image source={require('../../assets/images/cuidadores.png')} style={styles.icon} />
+        <Text style={styles.cardText}>Cuidadores</Text>
+      </TouchableOpacity>
+
+      {/* BOTÃO FAVORITOS */}
+      <TouchableOpacity style={styles.card}>
+        <Image source={require('../../assets/images/favoritos.png')} style={styles.icon} />
+        <Text style={styles.cardText}>Favoritos</Text>
+      </TouchableOpacity>
+
+      {/* BOTÃO PERFIL */}
+      <TouchableOpacity style={styles.card}>
+        <Image source={require('../../assets/images/perfilicon.png')} style={styles.icon} />   
+        <Text style={styles.cardText}>Seu Perfil</Text>
+      </TouchableOpacity>
+
+      {/* BOTÃO LINKAGEM */}
+      <TouchableOpacity style={styles.card}>
+        <Image source={require('../../assets/images/linkagem.png')} style={styles.icon} />
+        <Text style={styles.cardText}>Linkagem</Text>
+      </TouchableOpacity>
+
+      {/* BOTÃO CONVERSAS */}
+      <TouchableOpacity style={styles.card}>
+        <Image source={require('../../assets/images/conversas.png')} style={styles.icon} /> 
+        <Text style={styles.cardText}>Conversas</Text>
+      </TouchableOpacity>
+
+      {/* BOTÃO FALE CONOSCO */}
+      <TouchableOpacity style={styles.card}>
+        <Image source={require('../../assets/images/faleconosco.png')} style={styles.icon} />
+        <Text style={styles.cardText}>Fale Conosco</Text>
       </TouchableOpacity>
     </View>
 
-    <View style={styles.profile}>
-      <Image source={require('../../assets/images/perfil.png')} style={styles.profileImage} />
-      <Text style={styles.greeting}>Olá {usuario.nomeUsuario}</Text>
-    </View>
 
-    <ScrollView contentContainerStyle={{
-      flexGrow: 1,         
-      paddingBottom: Platform.OS === 'web' ? width * 0.1 : width * 0.2  ,   
-      paddingHorizontal: 10,
-      alignItems: 'center',
-    }} style={styles.content}>
-      
-      <View style={styles.card1}>
-        <View style={styles.cardRow}>
-          <Image source={require('../../assets/images/caregiver.png')} style={styles.icon} />
-          <Text style={styles.cardText}>Contrate um Cuidador</Text>
-        </View>
-        <TouchableOpacity 
-          style={styles.button1} 
-          onPress={() => navigation.navigate('Contratar')}
-        >
-          <Text style={styles.buttonText}>Contrate</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.card}>
-        <View style={styles.cardRow}>
-          <Image source={require('../../assets/images/heartcare.png')} style={styles.icon} />
-          <Text style={styles.cardText}>Cuidadores que você favoritou</Text>
-        </View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Favoritos</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.card}>
-        <View style={styles.cardRow}>
-          <Image source={require('../../assets/images/heartcare.png')} style={styles.icon} />
-          <Text style={styles.cardText}>Linkar com Familiar</Text>
-        </View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Linkagem </Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.card}>
-        <View style={styles.cardRow}>
-          <Image source={require('../../assets/images/perfilcard.png')} style={styles.icon} />
-          <Text style={styles.cardText}>Suas informações</Text>
-        </View>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Perfil</Text>
-        </TouchableOpacity>
-      </View>
-
-    </ScrollView>
-  </View>
-);
+  {/* BOTÃO DE SOM FIXO */}
+  <TouchableOpacity style={styles.soundButton} onPress={() => alert('Auxiliar auditivo')}>
+    <Image source={require('../../assets/images/audio.png')} style={styles.soundIcon} />
+  </TouchableOpacity>
+  </ScrollView>
+</View>
+  );
 }
 
 const styles = StyleSheet.create({
-container: { 
-  flex: 1, 
-  backgroundColor: '#fff' 
+  container: { 
+    flex: 1, 
+    backgroundColor: '#fff' 
+  },
+  Logo: {
+    left: -190,
+    top: -104,
+    position: 'absolute',
+  },
+  Form1: {
+    width: width * 0.9,
+    aspectRatio: 2,
+    backgroundColor: '#a4e9e5',
+    borderRadius: 9999,
+    transform: [{ scaleX: 1.3 }],
+    position: 'absolute',
+    top: -20,
+    left: -60,
+  },
+logout: {
+  backgroundColor: '#fff',
+  paddingHorizontal: 15,
+  paddingVertical: 8,
+  borderRadius: 15,
+  fontWeight: 'bold',
+  borderWidth: 1,
+  borderColor: '#ccc'
 },
-header: {
-  flexDirection: 'row',
-  justifyContent: 'space-between',
+grid: {
+  
+  marginTop: '55%',
   alignItems: 'center',
-  backgroundColor: colors.azul,
-  paddingHorizontal: 10,
-  paddingVertical: 5,        
-},
-logo: { 
-  width: 120, 
-  height: 120, 
-  resizeMode: 'contain' 
-},
-som: { 
-  width: 40, 
-  height: 40, 
-  resizeMode: 'contain' 
-},
-profile: { 
-  alignItems: 'center', 
-  marginVertical: 30
-},
-greeting: { 
-  fontSize: 18,
-  marginTop: 10 
-},
-profileImage: { 
-  width: 120, 
-  height: 120, 
-  resizeMode: 'contain', 
-  marginBottom: 10
-},
-content: {
-  paddingBottom: 20,
-},
-card1: {
-  width: 330,
-  borderWidth: 3,
-  borderColor: colors.preto,
-  borderRadius: 12,
-  padding: 20,
-  marginBottom: 15,
-  alignSelf: 'center',
-  backgroundColor: colors.branco,
-},
-  card: {
-  width: 300,
-  borderWidth: 3,
-  borderColor: '#000',
-  borderRadius: 12,
-  padding: 10,
-  marginBottom: 15,
-  alignSelf: 'center',
-  backgroundColor: '#f9f9f9',
-},
-cardRow: {
+  overflow: 'hidden',
+
   flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  gap: 15,
+},
+card: {
+ 
+  width: 160,
+  height: 130,
+  backgroundColor: '#a4e9e5',
+  borderRadius: 20,
   alignItems: 'center',
-  marginBottom: 10,
+  justifyContent: 'flex-end',
+  margin: 12,
+  elevation: 5,
+  paddingBottom: 15,
+  position: 'relative',
+  overflow: 'visible', // garante que o ícone pode “sair”
 },
 icon: { 
-  width: 70, 
-  height: 70, 
-  resizeMode: 'contain', 
+  width: 100, 
+  height: 100, 
+  resizeMode: 'contain',
+  position: 'absolute',
+  top: -23,   // faz o ícone "sair" para fora
 },
-cardText: { 
-  fontSize: 16, 
-  marginLeft: 10, 
-  flexShrink: 1,
-},
-button1: {
-  backgroundColor: colors.azul,
-  paddingVertical: 10,
-  marginTop: 10,
-  borderRadius: 10,
-  borderWidth: 3,
-  borderColor: colors.preto,
-  alignItems: 'center',
-},
-button: {
-  backgroundColor: colors.azul,
-  paddingHorizontal: 10,
-  paddingVertical: 5,
-  borderRadius: 10,
-  borderWidth: 3,
-  borderColor: '#000',
-  alignItems: 'center',
-},
-buttonText: { 
+cardText: {
+  fontSize: 14,
   fontWeight: 'bold',
+  textAlign: 'center',
+  marginTop: 30, // empurra o texto para baixo do ícone
 },
+soundButton: {
+  position: 'absolute',   // geralmente melhor para botões flutuantes
+  bottom: 30,
+  alignSelf: 'center',
+  width: 56,
+  height: 56,
+  borderRadius: 28,      // metade do tamanho → círculo perfeito
+  backgroundColor: '#fff',
+  padding: 5,            // remove espaço interno
+  alignItems: 'center',
+  justifyContent: 'center',
+  elevation: 10,
+ 
+},
+soundIcon: {
+  width: 80,
+  height: 80,
+ 
+}
 });
